e05102e94d76007b04117f8d022ff15a5bcb79eb
j053c4 <j053.c4@gmail.com>
2016-02-21 17:23:32 -0430

    inicie a trabajar congit mi proyecto

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>	
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Seemb</title>
+		<link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="publico/css/estilo-index.css">
+	</head>
+	<body id="body-index">
+		<br>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12" >
+					<!--<div class="page-header">-->
+					<header id="header-index">
+						<img src="publico/img/banner.jpg" class="img-responsive img-rounded img-banner" >
+					</header> <!--....header-->
+					<!--</div>-->
+				</div>
+			</div>
+		</div>
+		
+		
+			<div class="container">
+			<section id="section-index">
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4 ">
+					 	
+						 	<form class="form-signin">
+						 		<br>			 		
+						        <h2 class="form-signin-heading text-center">Por Favor Inicie Sesion</h2>
+						        
+						       <!-- <label for="inputEmail" class="sr-only"></label>-->
+						        <label for="nombre_de_usuario"> Usuario :</label>
+						        <br>
+						        <input type="text" id="inputusuario" class="form-control" placeholder="Introduce tu usuario" required autofocus>
+						        <br>
+						        <!--<label for="inputPassword" class="sr-only">Password</label>-->
+						         <label for="password"> Contraseña:</label>
+						        <br>
+						        <input type="password" id="inputPassword" class="form-control" placeholder="Introduce tu Contraseña" required>
+			        			<br>
+			        			<button class="btn btn-lg btn-success btn-block" type="submit">Iniciar Sesion</button>
+			        			<br><br><br>
+			     			</form>
+			     			
+					</div>	
+
+				</div>
+			</section> <!--section-->
+			</div>
+		
+		
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<footer id="pie-index" class="text-center" >
+							<!--<p class="text-center">Creado Bajo estandares GPL</p>-->
+							<button class="btn btn-lg btn-default btn-block "><p class="text-center "> <b>PHP5  PostgreSQL  CSS3  HTML5 </b></p></button>
+							
+						</footer>
+					</div>
+				</div>
+			</div>
+		
+	</body>		
+</html>
